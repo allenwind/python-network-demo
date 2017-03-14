8ce58fe1f63a4c6290b61f50ffebf74a1e3ff9c8
allenwind <allenwind@foxmail.com>
2017-03-14 22:06:43 +0800

    ipc sending fd

---
+++ ipc_for_fd/server.py
@@ -0,0 +1,29 @@
+import socket
+import struct
+
+def send_fd(sock, fd):
+    sock.sendmsg([b'x'],
+                 [(socket.SOL_SOCKET, socket.SCM_RIGHTS, struct.pack('i', fd))])
+
+    ack = sock.recv(2)
+    assert ack == b'OK'
+
+def server(work_address, port):
+    work_serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    work_serv.bind(work_address)
+    work_serv.listen(1)
+    worker, addr = work_serv.accept()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    s.bind(('', port))
+    s.listen(1)
+
+    while True:
+        client, addr = s.accept()
+        print("server: got connection from", addr)
+        send_fd(worker, client.fileno())
+        client.close()
+
+
+        
